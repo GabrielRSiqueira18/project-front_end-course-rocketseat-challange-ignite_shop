7f9bc5acaa1b59403d7f6abb4b2b770fd114ec6c
GabrielRSiqueira18 <128639376+GabrielRSiqueira18@users.noreply.github.com>
2023-05-09 11:35:11 -0300

    Finished

---
+++ src/styles/pages/app.ts
@@ -0,0 +1,18 @@
+import { styled } from "..";
+
+export const Container = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  minHeight: "100vh",
+  margin: "0 auto",
+})
+
+export const Header = styled("header", {
+  padding: "2rem 0",
+  width: "100%",
+  maxWidth: "1180px",
+  margin: "0 auto",
+  
+})
